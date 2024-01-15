e0597b34ceabe06173559f3989341779f53dea45
poonambharara87 <poonambharara87@gmail.com>
2024-01-15 11:19:58 +0530

    middleware

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>register</title>
+    </head>
+    <body>
+        <h1>Dashboard</h1>
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/register', function () { return view('register'); });
+Route::get('/login', function () { return view('login'); })->name('login');
+
+
+
+
+Route::middleware('auth:web')->group(function () {
+    Route::get('/dashboard', function(){ return view('/dashboard');});
+});
